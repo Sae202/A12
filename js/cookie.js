8120d446b041b7e1b65e35c5fe41d111fd079802
Sae202 <sfa25@njit.edu>
2022-11-23 00:11:57 -0500

    A12

---
+++ js/cookie.js
@@ -0,0 +1,11 @@
+
+    setCookie=(cName, cValue, expDays) => {
+    let date= new Date();
+    date.setTime(date.setTime()+(expDays * 24 *60 *60 *1000) );
+    const expires="expires=" + date.toUTCString();
+    document.cookie= cName + "=" + cValue +";" + expires + "; path=/"
+    }
+    document.querySelector(cookies-btn).addEventListener("click", () => {
+    document.querySelector("#cookies").style.display="none"
+    setCookie("cookie", true, 10)
+})
